4492f9f7ca35d0f219ea97e415c6df76fe251a1b
Merseong <esc990720@korea.ac.kr>
2023-05-17 13:55:23 +0900

    Fix lint settings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "LoginPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes></Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <p>This is header for all page</p>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="*" element="404 Not Found" />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/LoginPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoginPage() {
+  return <p>This is login page</p>;
+}
+
+export default LoginPage;
