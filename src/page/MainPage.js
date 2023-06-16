d120a6c7a18f0b4df3f386ff44901e5d0d341886
Merseong <esc990720@korea.ac.kr>
2023-06-16 13:49:57 +0900

    Add project create page

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "routes/paths";
+
+function LoginedMainPage() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <p>logined</p>
+      <Button onClick={() => navigate(PATHS.project.add)}>to Project Add</Button>
+    </>
+  );
+}
 
 function MainPage() {
   const logined = useSelector((state) => state.counter.logined);
 
-  return <Container>{logined ? <p>logined</p> : <p>not logined</p>}</Container>;
+  return <Container>{logined ? <LoginedMainPage /> : <p>not logined</p>}</Container>;
 }
 
 export default MainPage;
--- src/page/ProjectAddPage.js
+++ src/page/ProjectAddPage.js
@@ -1,7 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "routes/paths";
+import { fetchPost } from "utils/functions";
 
 function ProjectAddPage() {
-  return <p>ProjectAddPage</p>;
+  const navigate = useNavigate();
+  const [postingProcess, setPostingProcess] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  function handleFormValueChange(e) {
+    setFormData((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    if (postingProcess) return;
+    setPostingProcess(true);
+
+    // verification
+    if (!formData.Name?.length) {
+      setPostingProcess(false);
+      return;
+    }
+
+    if (formData.StartDate) {
+      formData.StartDate += "T00:00:000Z";
+    }
+
+    console.log(formData);
+
+    fetchPost("/project", formData)
+      .then((res) => {
+        if (!res.ok) {
+          switch (res.status) {
+            case 401:
+              navigate(PATHS.login);
+              throw new Error("허용되지 않은 접근입니다");
+            case 409:
+              throw new Error("같은 이름의 게임(프로젝트)이 이미 존재합니다");
+            default:
+              throw new Error("미확인 오류입니다. 관리자에게 문의해주세요");
+          }
+        }
+        alert("생성이 완료되었습니다");
+        navigate(PATHS.project.list);
+      })
+      .catch((err) => {
+        alert(err.message);
+      })
+      .finally(() => {
+        setPostingProcess(false);
+      });
+  }
+
+  return (
+    <Container>
+      <Form onSubmit={handleFormSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>
+            게임명 (프로젝트명) <span className="text-danger">*</span>
+          </Form.Label>
+          <Form.Control required name="Name" disabled={postingProcess} onChange={handleFormValueChange} />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>시작일</Form.Label>
+          <Form.Control name="StartDate" disabled={postingProcess} type="date" onChange={handleFormValueChange} />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>장르</Form.Label>
+          <Form.Control name="Genre" disabled={postingProcess} onChange={handleFormValueChange} />
+        </Form.Group>
+
+        <Button className="float-end" type="submit" disabled={postingProcess}>
+          저장
+        </Button>
+      </Form>
+    </Container>
+  );
 }
 
 export default ProjectAddPage;
