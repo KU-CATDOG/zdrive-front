9a75a0be8bf395603c738d5ea956b7980d5008c4
Merseong <esc990720@korea.ac.kr>
2023-06-19 21:35:49 +0900

    Add project list view

---
+++ src/components/ProjectListView.js
@@ -0,0 +1,66 @@
+import { map, slice } from "lodash";
+import React, { useEffect, useState } from "react";
+import { Button, Table } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "routes/paths";
+import { fetchGet } from "utils/functions";
+
+function ProjectListView({ fetchUrl = "/project/list?period=2023-1", cutoutCount = 5 }) {
+  const navigate = useNavigate();
+  const [currentList, setCurrentList] = useState([]);
+  const [cutouted, setCutouted] = useState(false);
+
+  function getProjectList() {
+    fetchGet(fetchUrl)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.length) {
+          if (data.length > cutoutCount) {
+            setCutouted(true);
+            slice(data, 0, cutoutCount);
+          }
+          setCurrentList(data);
+        }
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }
+
+  useEffect(() => {
+    getProjectList();
+  }, []);
+
+  return (
+    <>
+      <Table hover>
+        <thead>
+          <tr>
+            <th>이름</th>
+            <th>장르</th>
+          </tr>
+        </thead>
+        <tbody>
+          {map(currentList, (project) => (
+            <tr key={project.id} onClick={() => navigate(`${PATHS.project.detail}/${project.id}`)}>
+              <td>{project.name}</td>
+              <td>{project.genre}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      {cutouted && (
+        <Button size="sm" variant="link" className="float-end" onClick={() => navigate(PATHS.project.list)}>
+          더보기
+        </Button>
+      )}
+    </>
+  );
+}
+
+export default ProjectListView;
--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -1,15 +1,37 @@
+import ProjectListView from "components/ProjectListView";
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 function LoginedMainPage() {
-  return <p>logined</p>;
+  return (
+    <Row className="mt-4">
+      <Col md={6} className="p-2">
+        <div className="p-2" style={{ border: "1px solid lightgray" }}>
+          <h4 className="text-center">이번 분기의 프로젝트</h4>
+          <hr />
+          <ProjectListView fetchUrl="/project/list?period=2023-1" />
+        </div>
+      </Col>
+      <Col md={6} className="p-2">
+        <div className="p-2" style={{ border: "1px solid lightgray" }}>
+          <h4 className="text-center">내 프로젝트</h4>
+          <hr />
+          <ProjectListView fetchUrl="/project/list?period=2022-2" />
+        </div>
+      </Col>
+    </Row>
+  );
+}
+
+function UnauthMainPage() {
+  return <p>not logined</p>;
 }
 
 function MainPage() {
   const logined = useSelector((state) => state.counter.logined);
 
-  return <Container>{logined ? <LoginedMainPage /> : <p>not logined</p>}</Container>;
+  return <Container>{logined ? <LoginedMainPage /> : <UnauthMainPage />}</Container>;
 }
 
 export default MainPage;
