91d53e7779e3d13880053204c701fc5402f88c0b
Merseong <esc990720@korea.ac.kr>
2023-06-26 11:27:40 +0900

    Add fold on size in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Navbar, Nav, Container, Button, Form } from "react-bootstrap";
 import { fetchGet } from "utils/functions";
 import { PATHS } from "routes/paths";
 import { useDispatch, useSelector } from "react-redux";
@@ -52,34 +52,39 @@ function Header() {
   }, []);
 
   return (
-    <Navbar className="mb-2" bg="dark" variant="dark">
+    <Navbar className="mb-2" bg="dark" variant="dark" collapseOnSelect expand="md">
       <Container>
         <Navbar.Brand href="#m" onClick={() => navigate(PATHS.root)}>
           CATDOG Z-Drive
         </Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#m" onClick={() => navigate(PATHS.root)}>
-            메인
-          </Nav.Link>
-          <Nav.Link href="#p" onClick={() => navigate(PATHS.project.list)}>
-            탐색
-          </Nav.Link>
-          <Nav.Link href="#a" onClick={() => navigate(PATHS.about)}>
-            About Us
-          </Nav.Link>
-        </Nav>
-        <Nav>
-          <Navbar.Text className="me-3">{studentNumber}</Navbar.Text>
-          {logined ? (
-            <Button size="sm" variant="danger" onClick={handleLogout}>
-              로그아웃
-            </Button>
-          ) : (
-            <Button size="sm" variant="primary" onClick={handleLogin}>
-              로그인
-            </Button>
-          )}
-        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#m" onClick={() => navigate(PATHS.root)}>
+              메인
+            </Nav.Link>
+            <Nav.Link href="#p" onClick={() => navigate(PATHS.project.list)}>
+              탐색
+            </Nav.Link>
+            <Nav.Link href="#a" onClick={() => navigate(PATHS.about)}>
+              About Us
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Form className="ms-auto">
+              <Navbar.Text className="me-3">{studentNumber}</Navbar.Text>
+              {logined ? (
+                <Button type="button" size="sm" variant="danger" onClick={handleLogout}>
+                  로그아웃
+                </Button>
+              ) : (
+                <Button type="button" size="sm" variant="primary" onClick={handleLogin}>
+                  로그인
+                </Button>
+              )}
+            </Form>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
